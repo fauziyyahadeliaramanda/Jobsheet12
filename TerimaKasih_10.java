d3614443205faafef542746d4746ee7bd45f8d71
faujiiiahhh <framandaadelia@gmail.com>
2023-11-23 08:18:31 +0700

    Dengan Fungsi

--- TerimaKasih_10.java
+++ TerimaKasih_10.java
@@ -1,6 +1,9 @@
 public class TerimaKasih_10 {
-    public static void main(String[] args){
+    public static void Ucapanterimakasih(){
     System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    public static void main(String[] args) {
+    Ucapanterimakasih();
     }
     }
     
