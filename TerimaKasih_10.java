4c0b349d0af5faa2e33064fe861dda009e4b0f27
faujiiiahhh <framandaadelia@gmail.com>
2023-11-23 08:44:29 +0700

    Percobaan 3

--- TerimaKasih_10.java
+++ TerimaKasih_10.java
@@ -1,9 +1,16 @@
 public class TerimaKasih_10 {
     public static void Ucapanterimakasih(){
-    System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    System.out.println("Thank you for being the best teacher in the world.\n" +
+     "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
     }
     public static void main(String[] args) {
     Ucapanterimakasih();
+    String ucapan = "Terima Kasih Pak..Bu..Semoga Sehat Selalu";
+
+    UcapanTambahan(ucapan);
     }
-    }
+}
     
