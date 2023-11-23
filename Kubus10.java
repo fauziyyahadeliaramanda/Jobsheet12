c05e8d74c7a3c9459df978a273f8ee9fd44d0876
faujiiiahhh <framandaadelia@gmail.com>
2023-11-23 10:03:54 +0700

    Tugas No 1

---
+++ Kubus10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Kubus10 {
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int s, Lp, vol;
+
+        System.out.println("Masukkan sisi ");
+        s = input.nextInt();
+
+        Lp=hitungLuas(s);
+        System.out.println("Luas Permukaan kubus adalah " + Lp);
+        
+        vol=hitungVolume(s);
+        System.out.println("Volume kubus adalah " + vol);
+}
+    static int hitungLuas (int s){
+        int Luas = 6*s;
+        return Luas;
+}
+    static int hitungVolume (int s){
+        int volume = s*s*s;
+        return volume;
+        }
+ } 
